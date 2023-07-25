c0e7dac7d78eeba1fc7383c692276875eb4f5a96
Kan Sophan <ytbro65@gmail.com>
2023-07-25 11:41:57 +0700

    ready

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
+const multer = require('multer');
+
+// Replace with your MongoDB connection URL
 const uri = "mongodb+srv://sophan:sophan%40123@cluster0.r3agzsk.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(console.log("DB Connected"))
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage });
+
+// Define the Mongoose schema for the Image model
+var imageSchema = new mongoose.Schema({
+	name: String,
+	img:
+	{
+		data: Buffer,
+		contentType: String
+	},
+	price: Number
+});
+
+// Create the Image model using the schema
+var Image = mongoose.model('Image', imageSchema);
+
 app.get('/', async (req, res) => {
     try {
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         await client.connect();
         const database = client.db('test');
         const collection = database.collection('images');
@@ -19,22 +57,45 @@ app.get('/', async (req, res) => {
         await client.close();
     }
 });
-app.post('/upload', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db('test');
-        const collection = database.collection('images');
-        const { name, price, image } = req.body;
-        await collection.insertOne({ name, price, image });
-        res.status(201).send('Image uploaded successfully!');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred while uploading the image to the database.');
-    } finally {
-        await client.close();
-    }
+
+app.get('/', (req, res) => {
+	Image.find({})
+	.then((data, err)=>{
+		if(err){
+			console.log(err);
+			res.status(500).json({ error: err });
+		}
+		res.json(data);
+	})
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+
+app.post('/api/upload', upload.single('image'), (req, res, next) => {
+
+	var obj = {
+		name: req.body.name,
+		img: {
+			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+			contentType: 'image/png'
+		},
+		price: req.body.price
+	}
+	Image.create(obj)
+	.then ((item, err) => {
+		if (err) {
+			console.log(err);
+			res.status(500).json({ error: err });
+		}
+		else {
+			res.json(item);
+		}
+	});
 });
+
+// Replace with your desired port number
+var port = 3000;
+app.listen(port, err => {
+	if (err)
+		throw err
+	console.log('Server listening on port', port)
+})
